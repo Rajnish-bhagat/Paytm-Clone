adca12123105b3ad2b122280cbaaa51e6b9e3c6d
Rajnish Bhagat <rajnishbhagat4@gmail.com>
2024-07-16 00:08:08 +0530

    Completed the static frontend using tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
+import { Dashboard } from './pages/Dashboard';
+import { SendMoney } from './pages/SendMoney';
 
 function App() {
   return (
